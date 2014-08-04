f2f430c0bdd9bf59b2bd170e570e66c4bd7a8309
slamflipstrom <lindstrom.s@gmail.com>
2014-08-04 14:33:13 -0500

    Initial Commit

---
+++ lib/llama.rb
@@ -0,0 +1,9 @@
+#LlamaRanch/lib/llama.rb
+class Llama
+  attr_reader :name
+
+  def initialize( name )
+    @name = name
+  end
+
+end
---
+++ spec/lib/llama_ranch.rb
@@ -0,0 +1,11 @@
+# LlamaRanch/spec/lib/llama_spec.rb
+
+require "spec_helper" # this lets us run tests
+require "llama" # this is the class we're testing with
+
+describe Llama do
+  it "should have a name that we assign to it at creation" do
+    llama = Llama.new("Rocky")
+    expect(llama.name).to eql("Rocky")
+  end
+end
